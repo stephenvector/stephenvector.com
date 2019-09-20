ae73bef322dad368bddf8162910776744c7179b9
Stephen Vector <stephenvector@pm.me>
2019-09-19 20:50:00 -0400

    Updating font styling.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,21 @@ export default function Home() {
   return (
     <>
       <style jsx>{`
-        font-family: sans-serif;
+        @import url("https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700&display=swap");
+
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+
+        html {
+          box-sizing: border-box;
+        }
+        body {
+          padding: 1rem;
+          font-family: "IBM Plex Sans", sans-serif;
+        }
       `}</style>
       <h1>Stephen Vector</h1>
       <p>Fullstack Web Developer</p>
